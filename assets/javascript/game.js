88ffe2b6057ce332a0ed2b1b9ddfbf5f61341a0e
tanysaur <tanysaur@gmail.com>
2016-11-09 20:03:14 -0600

    working now, added hovers🍣🍣

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,15 +4,16 @@
 // numberOptions for each food: 1-12
 // counter: init to 0
 
-// prompt user to play
+// prompt user to play --> see canvas on W3
 
-// functions:
-// Check subTotalNumber < targetNumber
-// Check 
+// Functions:
+// Check counter < targetNumber
+// reset counters
+// play again
 
-// Assign random value to food (foodRandomValue)
-// User clicks on a food, add value subTotalNumber
-// checkSubTotal
+// Assign random value to food (numberOptions)
+// User clicks on a food, add value to counter
+// check counter
 
 var counter = 0;
 var wins = 0;
@@ -22,79 +23,73 @@ var sushiVal;
 var makiVal;
 var nigiriVal;
 var gyozaVal;
-var newGame = true;
-var targetNumber = 0;
+var sushiQ = 0;
+var makiQ = 0;
+var nigiriQ = 0;
+var gyozaQ = 0;
+
+var targetNumber = Math.floor((Math.random() * 120) + 19);
+		$(".targetNumber").text(targetNumber);
+
+resetCounters();
 
 
-$(document).ready(function() {
-	//start game
-	//gameStarts();
+	
+$("#sushi").on("click", function(){
+	counter += numberOptions[0];
+	sushiQ++;
+	$(".sushiQ").text(sushiQ);
+	checkCounterStats();
 })
 
-// function gameStarts(){
+$("#maki").on("click", function(){
+	counter += numberOptions[1];
+	makiQ++;
+	$(".makiQ").text(makiQ);
+	checkCounterStats();
+})
 
-	//try while loop
-	if(newGame){
-	
-		var targetNumber = Math.floor((Math.random() * 120) + 19);
-		$(".targetNumber").text(targetNumber);
-		for(i = 0; i < numberOptions.length; i++){
-			numberOptions[i] = Math.floor(Math.random()* 12) +1;
-			//$("#food").data(data-value, numberOptions[i]);
-		}
+$("#nigiri").on("click", function(){
+	counter += numberOptions[2];
+	nigiriQ++;
+	$(".nigiriQ").text(nigiriQ);
+	checkCounterStats();
+})
 
-		// $(".food").on("click", function(){
+$("#gyoza").on("click", function(){
+	counter += numberOptions[3];			
+	gyozaQ++;
+	$(".gyozaQ").text(gyozaQ);
+	checkCounterStats();
+})
 
-		// })
 
-		$("#sushi").on("click", function(){
-			counter += numberOptions[0];
-			var sushiQ = 0;
-			sushiQ++;
-			$(".sushiQ").text(sushiQ);
-			checkCounterStats();
-		})
+//this function resets the counters as well as their corresponding displays on the html
+function resetCounters(){
 
-		$("#maki").on("click", function(){
-			counter += numberOptions[1];
-			var makiQ = 0;
-			makiQ++;
-			$(".makiQ").text(makiQ);
-			checkCounterStats();
-		})
-
-		$("#nigiri").on("click", function(){
-			counter += numberOptions[2];
-			var nigiriQ = 0;
-			nigiriQ++;
-			$(".nigiriQ").text(nigiriQ);
-			checkCounterStats();
-		})
-
-		$("#gyoza").on("click", function(){
-			counter += numberOptions[3];
-			var gyozaQ = 0;
-			gyozaQ++;
-			$(".gyozaQ").text(gyozaQ);
-			checkCounterStats();
-		})
+	for(i = 0; i < numberOptions.length; i++){
+		numberOptions[i] = Math.floor(Math.random()* 12) +1;
 	}
-//}
 
-console.log(numberOptions);
+	var newTargetNumber = Math.floor((Math.random() * 120) + 19);
+		$(".targetNumber").text(newTargetNumber);
 
-// $(".food").on("click", function(){
-// 	// $("#food").attr(data-value, numberOptions[i]);
+	targetNumber = newTargetNumber;
+	console.log(numberOptions);
 
-// 	// console.log(numberOptions);
-// 	// counter = numberOption[i] + counter;
-// 	// checkCounterStats();
-// 	// console.log(counter);
+	$("#plateNumber").text(counter);
+	sushiQ = 0;
+	$(".sushiQ").html(sushiQ);
+	makiQ = 0;
+	$(".makiQ").text(makiQ);
+	nigiriQ = 0;
+	$(".nigiriQ").text(nigiriQ);
+	gyozaQ = 0;
+	$(".gyozaQ").text(gyozaQ);
 	
-// });
+}
 
-
-
+//this function checks if the user has reached the target number or if it has passed it
 function checkCounterStats(){
 	//when user reached your target number
 	if(counter == targetNumber){
@@ -105,14 +100,14 @@ function checkCounterStats(){
 	}
 	//when user's shopping cart sub total is still below your target / budget$
 	else if(counter < targetNumber){
-		console.log("keep adding to your plate");
+		//console.log("keep adding to your plate");
 		$("#plateNumber").text(counter);
 	} 
 	//when user has gone above the target / budget$
 	else if(counter > targetNumber){ //plate not yet complete
 		losses++;
 		$(".losses").text("Number of plates wasted: " + losses);
-		alert("You went over your budget!");
+		alert("You went over the budget by this much: $" + (counter - targetNumber));
 		playAgain();
 	}
 }
@@ -120,18 +115,13 @@ function checkCounterStats(){
 //asks user if they want to play again
 function playAgain(){
 	var playAgain =	confirm("Do you want to play again?")
-	if (playAgain){
-		counter = 0;
-		sushiVal = 0;
-		makiVal = 0;
-		nigiriVal = 0;
-		gyozaVal = 0;
-		wins = 0;
-		losses = 0;
-		newGame = true;
-
+	//if user hits "OK", then counters are reset
+	if (playAgain){	
+		counter = 0;	
+		resetCounters();
+	// if user hits "Cancel", 
 	} else {
 		alert("OK, see you again!");
+		//close window or show good by picture
 	}
-	
 }

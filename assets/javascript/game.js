3fc8b008cc2e8485ce08b03c3a82d6ecc22278fc
tanysaur <tanysaur@gmail.com>
2016-11-12 14:48:15 -0600

    📄 added comments + references

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,14 +5,14 @@
 // counter: init to 0
 
 // Functions:
-// check counter == , < , > targetNumber
-// reset counters
-// play again
+// Check counter == , < , > targetNumber
+// Reset counters
+// Play again
 
 // Assign random value for food (numberOptions)
 // User clicks on a food, then adds value to counter
-// check counter conditions in function above
-// if condition is met, prompt user if they want to play again
+// Check counter conditions in function above
+// If condition is met, prompt user if they want to play again
 
 var counter = 0;
 var wins = 0;
@@ -27,11 +27,16 @@ var makiQ = 0;
 var nigiriQ = 0;
 var gyozaQ = 0;
 
+//Initialize random target number whose value is between 19 and 120
 var targetNumber = Math.floor((Math.random() * 120) + 19);
 $(".targetNumber").text(targetNumber);
 
+//For a new game, reset all counters
 resetCounters();
 
+//Click events x 4 for each image element
+//Each image click increments the counter by the image value and also increments the quantity table by 1 
+//For each change in counter, it will call the function checkCounterStats
 $("#sushi").on("click", function(){
 	counter += numberOptions[0];
 	sushiQ++;
@@ -61,21 +66,21 @@ $("#gyoza").on("click", function(){
 })
 
 
-//this function resets the counters as well as their corresponding displays on the html
+//This function resets the counters as well as their corresponding displays on the html
 function resetCounters(){
-	//this re-assigns a random value to the four food listed / numberOptions[]
+	//This re-assigns a random value to the four food listed / numberOptions[]
 	for(i = 0; i < numberOptions.length; i++){
 		numberOptions[i] = Math.floor(Math.random()* 12) +1;
 	}
 
-	//this re-assigns a new amount / budget / targetNumber for the user
+	//This re-assigns a new amount / budget / targetNumber for the user
 	var newTargetNumber = Math.floor((Math.random() * 120) + 19);
 		$(".targetNumber").text(newTargetNumber);
 
 	targetNumber = newTargetNumber;
 	//console.log(numberOptions);
 	
-	//(optional) resets the Cart counter
+	//(optional) Resets the Cart counter
 	$("#plateNumber").text(counter);
 	sushiQ = 0;
 	$(".sushiQ").html(sushiQ);
@@ -88,21 +93,21 @@ function resetCounters(){
 	
 }
 
-//this function checks if the user has reached the target number or if it has passed it
+//This function checks if the user has reached the target number or if it has passed it
 function checkCounterStats(){
-	//when user reached your target number, increment win, alert user with the win, ask if they want to play again
+	//When user reached your target number, increment win, alert user with the win, ask if they want to play again
 	if(counter == targetNumber){
 		wins++;
 		alert("You have filled up your plate! Enjoy your food!");
 		$(".wins").text("Number of plates enjoyed: " + wins);
 		playAgain();
 	}
-	//when user's shopping cart sub total is still below your target / budget$
+	//When user's shopping cart sub total is still below your target / budget$
 	else if(counter < targetNumber){
 		//console.log("keep adding to your plate");
 		$("#plateNumber").text(counter);
 	} 
-	//when user has gone above the targetNumber / budget$, increment losses, ask user if they want to play again. 
+	//When user has gone above the targetNumber / budget$, increment losses, ask user if they want to play again. 
 	else if(counter > targetNumber){ 
 		losses++;
 		$(".losses").text("Number of plates to be washed: " + losses);
@@ -111,17 +116,17 @@ function checkCounterStats(){
 	}
 }
 
-//asks user if they want to play again
+//Asks user if they want to play again
 function playAgain(){
 	var playAgain =	confirm("Do you want to play again?")
-	//if user hits "OK", then counters are reset
+	//If user hits "OK", then counters are reset
 	if (playAgain){	
 		counter = 0;	
 		resetCounters();
-	// if user hits "Cancel", 
+	//If user hits "Cancel", 
 	} else {
 		alert("OK, see you again! This window will now close!");
 		window.close();
-		//close window or show good by picture
+		//Close window or show good by picture
 	}
 }
